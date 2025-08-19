2cc5d06e57d1ff853585fac2f0311a24bd7228ed
Lucas Henrique Pereira Alves <202506088201@alunos.estacio.br>
2025-08-19 18:19:41 +0000

    SUPER TRUNFO INTERMEDIARIO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+    char c1[20];
+    char c2[20];
+    int p1;
+    int p2;
+    int pt1;
+    int pt2;
+    float a1;
+    float a2;
+    float pib2;
+    float pib1;
+
+
+printf("Digite o nome da PRIMEIRA cidade:\n");
+scanf("%s",c1);
+printf("Digite a população dessa cidade:\n");
+scanf("%i", &p1);
+printf("Digite o tamanho em área do território da cidade:\n");
+scanf("%f",&a1);
+printf("Qual é o valor do PIB dessa cidade?:\n");
+scanf("%f", &pib1);
+printf("Quantidade de pontos turísticos dessa cidade:\n");
+scanf("%i", &pt1);
+
+printf("Digite o nome da SEGUNDA cidade:\n");
+scanf("%s",c2);
+printf("Digite a população dessa cidade:\n");
+scanf("%i", &p2);
+printf("Digite o tamanho em área do território da cidade:\n");
+scanf("%f",&a2);
+printf("Qual é o valor do PIB dessa cidade?:\n");
+scanf("%f", &pib2);
+printf("Quantidade de pontos turísticos dessa cidade:\n");
+scanf("%i", &pt2);
+
+printf("A cidade: %s\n",c1);
+printf("Tem o numero de habitantes igual a: %i\n",p1);
+printf("Área equivalente a: %f\n", a1);
+printf("Com o PIB de: %f\n", pib1);
+printf("E tem essa quantidade de pontos turísticos: %i\n",pt1);
+
+printf("A cidade: %s\n",c2);
+printf("Tem o numero de habitantes igual a: %i\n",p2);
+printf("Área equivalente a: %f\n", a2);
+printf("Com o PIB de: %f\n", pib2);
+printf("E tem essa quantidade de pontos turísticos: %i\n",pt2);
+        return 0;
 }
